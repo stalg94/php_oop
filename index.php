a9128ea84b870ee943d9315b356be29e9257fed8
stalg94 <stalg28@gmail.com>
2021-11-08 21:28:26 +0200

    Update index.php

--- index.php
+++ index.php
@@ -11,67 +11,25 @@
 
 <?php
 
-class User{
+//abstract - не могут быть созданы экземпляры чтобы  моделировать те или иные сущности
+
+abstract class User{
     public $name;
-    public $password;
-    public $email;
-    public $city;
+    public $status;
 
-    //konstruktor
-    function __construct($name,$password,$email,$city)
-    {
-     $this->name=$name;
-     $this->password=$password;
-     $this->email=$email;
-     $this->city=$city;
-    }
-
-    function getInfo(){
-
-        $information= "{$this->name}"." "."{$this->password}"." "."{$this->email}"." "."{$this->city}";
-        return $information;
-    }
-
+    abstract function getStatus();
 }
 
-$user1 = new User("Alex","123456","dsfd@sd.com","Narva");
-echo $user1->getInfo();
-
-echo "<br>";
-
-class Moderator extends User{
-    public $info;
-    public $rights;
-
-    function __construct($name,$password,$email,$city, $info, $rights)
-    {
-        parent:: __construct($name,$password,$email,$city);
-        $this->info=$info;
-        $this->rights=$rights;
-    }
-    function getInfo(){
-        $information= parent::getInfo();
-        $information .= " "."{$this->info}"." "."{$this->rights}";
-        return $information;
-    }
-
-} 
-$moder = new Moderator("Ivan", "sate28735","dsvd@dcsd.ee","Tallinn", "Moderator","tru");
-echo $moder->getInfo();
-echo "<br>";
-class Test {
-    protected $info; //можем еги использовать но не можем его менять
-}
-
-class Test2 extends Test{
-    public function test(){
-        $this->info="info";
-        echo $this->info;
+class Admin extends User{//дочерний класс
+    function getStatus(){
+        echo "Admin";
     }
 }
-$test2 = new Test2;
-$test2->test();
-$test2->info="information";
+
+$user = new Admin;
+$user->getStatus();
+
+
 ?>
 </body>
 </html>
